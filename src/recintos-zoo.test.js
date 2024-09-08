18e3966fe27b4a8ee298c0d74a099a7691502431
Matheus Maia <irmadalava@gmail.com>
2024-09-08 17:01:38 -0300

    Upgrade para garantir que todos os recintos viaveis sejam testados pelo comprimento do array

--- src/recintos-zoo.test.js
+++ src/recintos-zoo.test.js
@@ -26,49 +26,58 @@ describe('Recintos do Zoologico', () => {
     test('Deve encontrar recinto para 1 crocodilo', () => {
         const resultado = new RecintosZoo().analisaRecintos('CROCODILO', 1);
         expect(resultado.erro).toBeFalsy();
+        expect(resultado.recintosViaveis).toHaveLength(1);
         expect(resultado.recintosViaveis[0]).toBe('Recinto 4 (espaço livre: 5 total: 8)');
-        expect(resultado.recintosViaveis.length).toBe(1);
     });
 
     test('Deve encontrar recintos para 2 macacos', () => {
         const resultado = new RecintosZoo().analisaRecintos('MACACO', 2);
         expect(resultado.erro).toBeFalsy();
-        expect(resultado.recintosViaveis[0]).toBe('Recinto 1 (espaço livre: 5 total: 10)');
-        expect(resultado.recintosViaveis[1]).toBe('Recinto 2 (espaço livre: 3 total: 5)');
-        expect(resultado.recintosViaveis[2]).toBe('Recinto 3 (espaço livre: 2 total: 7)');
-        expect(resultado.recintosViaveis.length).toBe(3);
+        expect(resultado.recintosViaveis).toHaveLength(3);
+        expect(resultado.recintosViaveis).toEqual([
+            'Recinto 1 (espaço livre: 5 total: 10)',
+            'Recinto 2 (espaço livre: 3 total: 5)',
+            'Recinto 3 (espaço livre: 2 total: 7)'
+        ]);
     });
 
     // Testes de regras específicas
     test('Carnívoros só podem dividir espaço com a própria espécie', () => {
         const resultado = new RecintosZoo().analisaRecintos('LEOPARDO', 1);
         expect(resultado.erro).toBeFalsy();
+        expect(resultado.recintosViaveis).toHaveLength(1);
         expect(resultado.recintosViaveis[0]).toBe('Recinto 5 (espaço livre: 3 total: 9)');
-        expect(resultado.recintosViaveis.length).toBe(1);
     });
 
     test('Hipopótamos só aceitam outras espécies em savanas com rio', () => {
         const resultado = new RecintosZoo().analisaRecintos('HIPOPOTAMO', 1);
         expect(resultado.erro).toBeFalsy();
-        expect(resultado.recintosViaveis[0]).toBe('Recinto 3 (espaço livre: 0 total: 7)');
+        expect(resultado.recintosViaveis).toHaveLength(2);
+        expect(resultado.recintosViaveis).toEqual([
+            'Recinto 3 (espaço livre: 0 total: 7)',
+            "Recinto 4 (espaço livre: 4 total: 8)"
+        ]);
     });
 
     test('Macacos não se sentem confortáveis sozinhos', () => {
         const resultado = new RecintosZoo().analisaRecintos('MACACO', 1);
         expect(resultado.erro).toBeFalsy();
-        expect(resultado.recintosViaveis[0]).toBe('Recinto 1 (espaço livre: 6 total: 10)');
-        expect(resultado.recintosViaveis[1]).toBe('Recinto 3 (espaço livre: 3 total: 7)');
-        expect(resultado.recintosViaveis.length).toBe(2);
+        expect(resultado.recintosViaveis).toHaveLength(2);
+        expect(resultado.recintosViaveis).toEqual([
+            'Recinto 1 (espaço livre: 6 total: 10)',
+            'Recinto 3 (espaço livre: 3 total: 7)'
+        ]);
     });
 
     // Teste de bugs
     test('Deve considerar espaço adicional para múltiplas espécies', () => {
         const resultado = new RecintosZoo().analisaRecintos('GAZELA', 1);
         expect(resultado.erro).toBeFalsy();
-        expect(resultado.recintosViaveis[0]).toBe('Recinto 1 (espaço livre: 4 total: 10)');
-        expect(resultado.recintosViaveis[1]).toBe('Recinto 3 (espaço livre: 3 total: 7)');
-        expect(resultado.recintosViaveis.length).toBe(2);
-
+        expect(resultado.recintosViaveis).toHaveLength(2);
+        expect(resultado.recintosViaveis).toEqual([
+            'Recinto 1 (espaço livre: 4 total: 10)',
+            'Recinto 3 (espaço livre: 3 total: 7)'
+        ]);
     });
 
 });
